007f5a9f9951acbbbe9158d38683104fcd750158
noman <nomanpc13@gmail.com>
2021-04-09 00:16:58 +0200

    auth system

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const { collection } = require('./app/models/menu')
 const MongoDbStore = require('connect-mongo')
-
+const passport = require('passport')
 
 //Database connection
 const url = 'mongodb://localhost/pizza';
@@ -22,6 +22,8 @@ connection.once('open', () => {
 }).catch(err => {
     console.log('Connction fail...')
 });
+
+
 
 //Session store
 let mongoStore = new MongoDbStore({
@@ -38,17 +40,25 @@ app.use(session({
 
 }))
 
+// Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 //Assets
 app.use(express.static('public'))
 //Global middleware 
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 // set Template engine
 app.use(expresslayout)
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
